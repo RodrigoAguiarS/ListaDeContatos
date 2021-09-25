9c96707d0d591cc9dc97749558f864f5c4ffddb1
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-09-25 14:01:49 -0300

    Options Menus

--- app/src/main/java/com/rodrigo/listadecontatos/MainActivity.kt
+++ app/src/main/java/com/rodrigo/listadecontatos/MainActivity.kt
@@ -2,6 +2,10 @@ package com.rodrigo.listadecontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -28,10 +32,34 @@ class MainActivity : AppCompatActivity() {
             arrayListOf(
                 Contato(
                     "Rodrigo Aguiar",
-                    "84 987061013",
+                    "(84) 98706-1013",
                     "img_png"
                 )
             )
         )
+    }
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+        //return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.itMenu1 -> {
+                showToast("Clicou no Menu1")
+                return true
+            }
+            R.id.itMenu2 -> {
+                showToast("Clicou no Menu2")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
