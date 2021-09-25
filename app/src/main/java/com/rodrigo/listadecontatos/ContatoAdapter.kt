eac9a4c0c536c382645c73b7a87429b0e1c1f4e8
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-09-24 23:21:38 -0300

    Desenvolvendo a classe ContactAdapter com as informações de cada card

--- app/src/main/java/com/rodrigo/listadecontatos/ContatoAdapter.kt
+++ app/src/main/java/com/rodrigo/listadecontatos/ContatoAdapter.kt
@@ -3,26 +3,37 @@ package com.rodrigo.listadecontatos
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ContatosAdapter : RecyclerView.Adapter<ContatosAdapter.ContatosAdapterViewHolder>(){
+class ContatoAdapter : RecyclerView.Adapter<ContatoAdapter.ContatosAdapterViewHolder>(){
+
     private val list: MutableList<Contato> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContatosAdapterViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contato_item, parent, false)
         return ContatosAdapterViewHolder(view)
     }
-
     override fun onBindViewHolder(holder: ContatosAdapterViewHolder, position: Int) {
         holder.bind(list[position])
     }
-
     override fun getItemCount(): Int {
         return list.size
     }
+    fun upDateList(list: List<Contato>){
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
     class ContatosAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun bind(contato: Contato){
+        private val txtNome: TextView = itemView.findViewById(R.id.txtNome)
+        private val txtTelefone: TextView = itemView.findViewById(R.id.txtTelefone)
+        private val imgFoto: ImageView = itemView.findViewById(R.id.imgFoto)
 
+        fun bind(contato: Contato){
+            txtNome.text = contato.nome
+            txtTelefone.text = contato.telefone
         }
     }
 }
--- app/src/main/java/com/rodrigo/listadecontatos/MainActivity.kt
+++ app/src/main/java/com/rodrigo/listadecontatos/MainActivity.kt
@@ -2,10 +2,36 @@ package com.rodrigo.listadecontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy{
+        findViewById<RecyclerView>(R.id.rv_List)
+    }
+    private val adapter = ContatoAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindView()
+        upDateList()
+    }
+    private fun bindView(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+
+    }
+    private fun upDateList(){
+        adapter.upDateList(
+            arrayListOf(
+                Contato(
+                    "Rodrigo Aguiar",
+                    "84 987061013",
+                    "img_png"
+                )
+            )
+        )
     }
 }
