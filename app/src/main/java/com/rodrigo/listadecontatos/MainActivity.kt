cede699f242fedb104ac7296a6bd0d0c0ef55bda
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-11-20 12:39:01 -0300

    Armazenando e acessando dados com SharedPreferences

--- app/src/main/java/com/rodrigo/listadecontatos/MainActivity.kt
+++ app/src/main/java/com/rodrigo/listadecontatos/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.rodrigo.listadecontatos
 
 //import android.widget.Toolbar
+
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-
+import androidx.core.content.edit
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
+import com.google.gson.Gson
 import com.rodrigo.listadecontatos.ContatoDetail.Companion.EXTRA_CONTATO
 
 
@@ -30,9 +33,26 @@ class MainActivity : AppCompatActivity(), ClickItemContatoListener {
         setContentView(R.layout.drawer_menu)
 
         initDrawer()
+        fetchListContact()
         bindView()
-        upDateList()
 
+
+    }
+    private fun fetchListContact(){
+        val list = arrayListOf(
+            Contato(
+                "Rodrigo Aguiar",
+                "(84) 98706-1013",
+                "img_png"
+            )
+        )
+        getInstanceSharedPreference().edit{
+            putString("contato", Gson().toJson(list))
+
+        }
+    }
+    private fun getInstanceSharedPreference(): SharedPreferences{
+        return getSharedPreferences("com.rodrigo.listadecontatos.PREFERENCES", MODE_PRIVATE)
     }
     private fun initDrawer(){
 
@@ -49,18 +69,17 @@ class MainActivity : AppCompatActivity(), ClickItemContatoListener {
     private fun bindView(){
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
+        upDateList()
+
+    }
+    private fun getListContatos(): List<Contato> {
+        val list = getInstanceSharedPreference().getString("contato","[]")
+        val turnsType = object : TypeToken<List<Contato>>() {}.type
+        return Gson().fromJson(list, turnsType)
 
     }
     private fun upDateList(){
-        adapter.upDateList(
-            arrayListOf(
-                Contato(
-                    "Rodrigo Aguiar",
-                    "(84) 98706-1013",
-                    "img_png"
-                )
-            )
-        )
+        adapter.upDateList(getListContatos())
     }
     private fun showToast(message: String){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
